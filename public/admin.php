fff09a98f493a656b46ef7619c61f2b1256a9eeb
rahul <rahul.yadav@targetintegration.com>
2014-06-20 18:48:03 +0530

    cms project

---
+++ public/admin.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>lynda</title>
+<link href="styleseets/public.css" media="all" rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+<div id="header">
+<h1>lynda</h1>
+</div>
+<div id="main">
+<div id="navigation">
+&nbsp
+</div>
+<div id="page">
+<h2>admin menu</h2>
+<p>Welcome to the lynda.</p>
+<ul>
+<li><a href="manage_content.php"> Manage website content </a></li>
+<li><a href="manage_admins.php">Manage Admin User </a></li>
+<li><a href="logout.php">Logout </a></li>
+</ul>
+</div>
+</div>
+<div id="footer">copyright 2014, lynda</div>
+</body>
+</html>
